e1c090e53cb7b7944605f23cd73ee1851fd0456c
avisavla <avisavla27@gmail.com>
2026-01-17 17:34:28 +0530

    Initial backend setup with user entity, registration, and login logic

---
+++ TaskManagementSystem/Model/DTO/RegisterDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementSystem.Model.DTO
+{
+    public class RegisterDTO
+    {
+        [Required, EmailAddress, MaxLength(256)]
+        public string Email { get; set; }
+
+        [Required,MaxLength(256)]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ TaskManagementSystem/Model/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementSystem.Model.Entities
+{
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(UserName), IsUnique = true)]
+
+    public class User
+    {
+        public int Id { get; set; }
+        
+        [MaxLength(256)]
+        public string Email { get; set; } = null!;  
+        
+        [MaxLength(256)]
+        public string UserName { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }     
+        public DateTime? LastLoginAt { get; set; }  
+
+        public bool IsActive { get; set; } = true;  
+    }
+}
