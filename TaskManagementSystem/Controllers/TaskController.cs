d2afee86fa45b7eaa3bffc80c972ac7857bf784f
avisavla <avisavla27@gmail.com>
2026-01-20 17:42:57 +0530

    Implemented Update and Delete endpoints with ownership checks

--- TaskManagementSystem/Controllers/TaskController.cs
+++ TaskManagementSystem/Controllers/TaskController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Win32;
 using System.Security.Claims;
 using TaskManagementSystem.Data;
+using TaskManagementSystem.Enum;
 using TaskManagementSystem.Model.DTO;
 using TaskManagementSystem.Model.Entities;
 using TaskManagementSystem.Services;
@@ -22,7 +23,7 @@ namespace TaskManagementSystem.Controllers
         }
 
         [Authorize]
-        [HttpPost("CreateTask")]
+        [HttpPost]
         public async Task<IActionResult> CreateTask(CreateTaskItemDTO dto)
         {
             if (!ModelState.IsValid)
@@ -30,7 +31,7 @@ namespace TaskManagementSystem.Controllers
                 return BadRequest(ModelState);
             }
 
-            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!); 
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
 
             var taskItemEntity = new TaskItem
             {
@@ -52,29 +53,96 @@ namespace TaskManagementSystem.Controllers
         }
 
         [Authorize]
-        [HttpGet("{Id}")]
-        public async Task<IActionResult> GetTaskById(int Id)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTaskById(int id)
         {
             var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
 
-            var task = await dbContext.Tasks.FindAsync(Id);
+            var task = await dbContext.Tasks.FindAsync(id);
 
             if (task == null)
             {
                 return NotFound();
             }
 
-            if (task.UserId != userId) 
-            { 
+            if (task.UserId != userId)
+            {
                 return Forbid();
             }
 
-            return Ok(new TaskItemDTO{
+            return Ok(new TaskItemDTO {
                 Id = task.Id,
                 Name = task.Name,
                 Description = task.Description,
                 Status = task.Status
             });
+        }
+
+        [Authorize]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTask(int id, TaskItemDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (dto.Id != id)
+                return BadRequest("Task ID mismatch");
+
+            if (!System.Enum.IsDefined(typeof(Status),dto.Status))
+            {
+                return BadRequest("Invalid task status value");
+            }
+
+
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+            var task = await dbContext.Tasks.FindAsync(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            if (task.UserId != userId)
+            {
+                return Forbid();
+            }
+
+            task.Name = dto.Name;
+            task.Description = dto.Description;
+            task.Status = dto.Status;
+            task.LastUpdatedDate = DateTime.Now;
+
+
+            await dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+            var task = await dbContext.Tasks.FindAsync(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            if (task.UserId != userId)
+            {
+                return Forbid();
+            }
+
+            dbContext.Tasks.Remove(task);
+            await dbContext.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
