3909837985f90201b0176b754a207e39cf6fbc94
avisavla <avisavla27@gmail.com>
2026-03-08 09:19:26 +0530

    Increased length for task description

---
+++ TaskManagementSystem/Migrations/20260308032550_UpdateTaskDescriptionLength.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTaskDescriptionLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tasks",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tasks",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000);
+        }
+    }
+}
--- TaskManagementSystem/Model/Entities/TaskItem.cs
+++ TaskManagementSystem/Model/Entities/TaskItem.cs
@@ -6,9 +6,9 @@ namespace TaskManagementSystem.Model.Entities
     public class TaskItem
     {
         public int Id { get; set; }
-        [MaxLength(1000)]
-        public string Name { get; set; } = null!;
         [MaxLength(200)]
+        public string Name { get; set; } = null!;
+        [MaxLength(3000)]
         public string Description { get; set; } = null!;
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public DateTime? LastUpdatedDate { get; set; }
